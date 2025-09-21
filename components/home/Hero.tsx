a99dae1a274f2d17e6377bce6e9b0e130689125c
Ryan Bakker <112455939+ryanbakker@users.noreply.github.com>
2025-09-22 07:00:09 +1200

    Add Heading CSS

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -13,7 +13,7 @@ function Hero() {
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="flex flex-col justify-start flex-1">
             <FadeInUp delay={0}>
-              <h1 className="text-[36px] sm:text-5xl lg:text-6xl xl:text-7xl font-extrabold leading-12 sm:leading-16 md:leading-20 tracking-tight page-content bg-gradient-to-tr from-neutral-900 via-neutral-800 to-neutral-600 dark:from-neutral-100/90 dark:via-white/90 dark:to-white/80 bg-clip-text text-transparent drop-shadow-[0_1px_1px_rgba(0,0,0,0.35)]">
+              <h1 className="top-heading-sizes font-extrabold leading-12 sm:leading-16 md:leading-20 tracking-tight page-content bg-gradient-to-tr from-neutral-900 via-neutral-800 to-neutral-600 dark:from-neutral-100/90 dark:via-white/90 dark:to-white/80 bg-clip-text text-transparent drop-shadow-[0_1px_1px_rgba(0,0,0,0.35)]">
                 Hi I&apos;m Ryan, <br />a Web Developer
               </h1>
             </FadeInUp>
